69d8ce96e5304929fd301a5e6aef6711a28c8cc1
Muhammad Ahmed Asim <ahmedniz72@gmail.com>
2026-02-19 01:58:18 +0500

    Add modulo function

--- calculator.py
+++ calculator.py
@@ -12,6 +12,8 @@ def divide(a, b):
     if b == 0:
         return "ERROR: Division by zero"
     return a / b
+def modulo(a, b):
+    return a % b
 
 
 print("Simple Calculator")
@@ -19,3 +21,4 @@ print("5+3 = ", add(5,3))
 print("23-2 = ",subtract(23,2))
 print("6 * 7 =", multiply(6, 7))
 print("20 / 5 =", divide(20, 5))
+print("10 % 3 =", modulo(10, 3))
